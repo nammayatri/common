bda7c4f690410b5de32f9cb27b93cfdf128b5d7c
Sridhar Ratnakumar <srid@srid.ca>
2023-03-03 10:43:56 -0500

    Import haskell-flake too

--- flake-module.nix
+++ flake-module.nix
@@ -4,6 +4,7 @@ common:
 {
   imports = [
     common.inputs.flake-root.flakeModule
+    common.inputs.haskell-flake.flakeModule
     (import ./nix/treefmt.nix common)
     (import ./nix/haskell.nix common)
     ./nix/ghc810.nix
