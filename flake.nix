6aeff8ef9500814227d542c9d1a5244c4c20d8a0
Sridhar Ratnakumar <srid@srid.ca>
2023-04-19 15:03:01 -0400

    haskell-flake: back to master

--- flake.nix
+++ flake.nix
@@ -4,8 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
-    # https://github.com/srid/haskell-flake/pull/137
-    haskell-flake.url = "github:sbh69840/haskell-flake/poc-localapps";
+    haskell-flake.url = "github:srid/haskell-flake";
     flake-root.url = "github:srid/flake-root";
     nixpkgs-21_11.url = "github:nixos/nixpkgs/nixos-21.11"; # Used for ormolu
     nixpkgs-140774-workaround.url = "github:srid/nixpkgs-140774-workaround";
