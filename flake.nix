c3b5ad9da1c90f01f486cc65179a80dafbfd953e
Sridhar Ratnakumar <srid@srid.ca>
2023-04-22 15:29:30 -0400

    Allow using new ormolu

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
     flake-root.url = "github:srid/flake-root";
-    nixpkgs-21_11.url = "github:nixos/nixpkgs/nixos-21.11"; # Used for ormolu
     nixpkgs-140774-workaround.url = "github:srid/nixpkgs-140774-workaround";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     cachix-push.url = "github:juspay/cachix-push";
