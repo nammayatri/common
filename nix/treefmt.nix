5439fb1a061a83628d2b48e5183a9a4ad389f399
Sridhar Ratnakumar <srid@srid.ca>
2023-04-22 15:39:20 -0400

    even older

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -15,7 +15,7 @@ common:
       # FIXME: Disabled until https://github.com/nammayatri/nammayatri/issues/31
       # programs.hlint.enable = true;
       programs.ormolu.enable = true;
-      programs.ormolu.package = pkgs.haskellPackages.ormolu_0_2_0_0;
+      programs.ormolu.package = pkgs.haskellPackages.ormolu_0_1_4_1;
 
       settings.formatter.ormolu = {
         options = [
