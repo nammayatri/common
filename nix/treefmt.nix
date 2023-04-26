8dade753a6b3fae6dbf7caace842b3cfc06e6422
Sridhar Ratnakumar <srid@srid.ca>
2023-04-26 15:16:39 -0400

    Re-indent

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -7,28 +7,30 @@ common:
     common.inputs.treefmt-nix.flakeModule
   ];
   perSystem = { self', config, pkgs, lib, system, ... }: {
-    treefmt.config = {
-      inherit (config.flake-root) projectRootFile;
-      package = pkgs.treefmt;
+    treefmt = {
+      config = {
+        inherit (config.flake-root) projectRootFile;
+        package = pkgs.treefmt;
 
-      programs.nixpkgs-fmt.enable = true;
-      # FIXME: Disabled until https://github.com/nammayatri/nammayatri/issues/31
-      # programs.hlint.enable = true;
-      programs.ormolu.enable = true;
+        programs.nixpkgs-fmt.enable = true;
+        # FIXME: Disabled until https://github.com/nammayatri/nammayatri/issues/31
+        # programs.hlint.enable = true;
+        programs.ormolu.enable = true;
 
-      programs.ormolu.package =
-        let pkgs-21_11 = common.inputs.nixpkgs-21_11.legacyPackages.${system};
-        in common.inputs.nixpkgs-140774-workaround.patch pkgs-21_11 pkgs-21_11.haskellPackages.ormolu;
-      settings.formatter.ormolu = {
-        options = [
-          "--ghc-opt"
-          "-XTypeApplications"
-          "--ghc-opt"
-          "-fplugin=RecordDotPreprocessor"
-        ];
+        programs.ormolu.package =
+          let pkgs-21_11 = common.inputs.nixpkgs-21_11.legacyPackages.${system};
+          in common.inputs.nixpkgs-140774-workaround.patch pkgs-21_11 pkgs-21_11.haskellPackages.ormolu;
+        settings.formatter.ormolu = {
+          options = [
+            "--ghc-opt"
+            "-XTypeApplications"
+            "--ghc-opt"
+            "-fplugin=RecordDotPreprocessor"
+          ];
+        };
+
+        programs.dhall.enable = true;
       };
-
-      programs.dhall.enable = true;
     };
 
     # Add it to the default Haskell project shell.
