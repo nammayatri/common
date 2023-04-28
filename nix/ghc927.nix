a5c6e2d291afe795bd9651f5699732bd57de85c6
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-04-28 16:34:31 +0530

    Adds new custom GHC-9.2.7 nix package set
    * Adds the custom package set for GHC-9.2.7
    
    * This tracks specifically the `ghc927` haskell nix packages set.
    
    * This package set is meant to be used with the nammayatri set of projects.
      making ghc-9.2.7 and the accompanying package set the default
      going forward.
    
    * This is meant to upgrade the previous ghc810 package stack.

---
+++ nix/ghc927.nix
@@ -0,0 +1,46 @@
+# To use this package set in your `haskell-flake` projects, set the
+# `basePackages` option as follows:
+#
+# > basePackages = config.haskellProjects.ghc927.outputs.finalPackages;
+#
+{ self, lib, ... }:
+
+let
+  # A function that enables us to write `foo = [ dontCheck ]` instead of `foo =
+  # lib.pipe super.foo [ dontCheck ]` in haskell-flake's `overrides`.
+  compilePipe = f: self: super:
+    lib.mapAttrs
+      (name: value:
+        if lib.isList value then
+          lib.pipe super.${name} value
+        else
+          value
+      )
+      (f self super);
+in
+{
+  perSystem = { pkgs, lib, config, ... }: {
+    haskellProjects.ghc927 = {
+      # This is not a local project, so disable those options.
+      packages = { };
+      devShell.enable = false;
+      autoWire = [ ];
+
+      # Uses GHC-9.2.7 package set as base
+      basePackages = pkgs.haskell.packages.ghc927;
+
+      source-overrides = {
+        # Dependencies from Hackage
+
+      };
+
+      overrides = compilePipe (self: super: with pkgs.haskell.lib.compose; {
+        binary-parsers = [ unmarkBroken doJailbreak ];
+        mysql-haskell = [ doJailbreak ];
+        prometheus-proc = [ unmarkBroken doJailbreak ];
+        lrucaching = [ unmarkBroken doJailbreak ];
+        wire-streams = [doJailbreak];
+      });
+    };
+  };
+}
