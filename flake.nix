5185f271bc39c6fc7ce94dbde42a3788e3a6958e
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-05-04 20:44:03 +0530

    Mobility GHC-9.2.7 upgrade (#5)
    * Adds new custom GHC-9.2.7 nix package set
    
    * Adds the custom package set for GHC-9.2.7
    
    * This tracks specifically the `ghc927` haskell nix packages set.
    
    * This package set is meant to be used with the nammayatri set of projects.
      making ghc-9.2.7 and the accompanying package set the default
      going forward.
    
    * This is meant to upgrade the previous ghc810 package stack.
    
    * Make the newly added GHC-9.2.7 package set the default in nix/haskell.nix
    
    * Makes the newly added GHC-9.2.7 the default `basePackages` set
      in nix/haskell.nix file
    
    * This commit replaces the old gch810 set with the new ghc927 set.
    
    * Replace ghc810 artifacts with ghc927 in the common flake/modules.
    
    * Replaces the old ghc810 artifacts and references in the codebase
      with the newer ghc927 ones, effectively replacing them.
    
    * "flake.nix" now exports `ghc927` instead of `ghc810` fields/attribute
      in flakeModules.
    
    * The older ghc810 references have been commented out across the codebase,
      to be deprecated and removed in the future.
    
    * Updates the flake.lock file
    
    * Commits the updated flake.lock file for the project
    
    * Updates Readme
    
    * Adds GHC-9.2.7 package set reference in `what's provided` section
    
    * Updates readme with 810 deprecation note
    
    * Revert "Updates the flake.lock file"
    
    * This reverts commit 249e4e093f43f7e86998b805ecc821924b1b2751
    
    * It was requested on the PR feedback at:-
      https://github.com/nammayatri/common/pull/5#discussion_r1183727639
    
    * Adds a comment wrt to the package-set version
    
    * Adds a comment w.r.t to the package-set version choice.
    
    * This was requested as a PR feedback, found here:-
      https://github.com/nammayatri/common/pull/5#discussion_r1183729847

--- flake.nix
+++ flake.nix
@@ -19,7 +19,8 @@
   outputs = inputs: {
     flakeModules = {
       default = import ./flake-module.nix { inherit inputs; };
-      ghc810 = ./nix/ghc810.nix;
+      # ghc810 = ./nix/ghc810.nix;
+      ghc927 = ./nix/ghc927.nix;
     };
   };
 }
