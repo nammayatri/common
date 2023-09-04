90bf3fbc4d9a6bf2251860192f37a9b524984168
Sridhar Ratnakumar <srid@srid.ca>
2023-09-04 12:03:58 -0400

    last arg

--- nix/haskell/hpack.nix
+++ nix/haskell/hpack.nix
@@ -4,16 +4,16 @@ writeShellApplication {
   name = "hpack";
   text = ''
     set -x
-    
+
     DIR=$(mktemp -d)
-    cp "$1" "$DIR"/package.yaml
+    cp "''${@: -1}" "$DIR"/package.yaml
 
     # Merge defaults with package.yaml
-    cat ${../../.hpack/defaults.yaml} "$DIR"/package.yaml > "$1"
-    ${lib.getExe hpack} "$1"
+    cat ${../../.hpack/defaults.yaml} "$DIR"/package.yaml > "''${@: -1}"
+    ${lib.getExe hpack} "$@"
 
     # Restore original
-    cp "$DIR"/package.yaml "$1"
+    cp "$DIR"/package.yaml "''${@: -1}"
     rm -rf "$DIR"
   '';
 }
