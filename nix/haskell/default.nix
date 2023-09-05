ad3b1ea0325ebf1e182f4450180bf7df6a813683
Sridhar Ratnakumar <srid@srid.ca>
2023-09-05 19:20:05 -0400

    Add default haskell package configuration

--- nix/haskell/default.nix
+++ nix/haskell/default.nix
@@ -6,6 +6,20 @@
         ./devtools.nix
       ];
       basePackages = config.haskellProjects.ghc810.outputs.finalPackages;
+      defaults.settings.default = { name, package, config, ... }:
+        lib.optionalAttrs (package.local.toDefinedProject or false) {
+          # Disabling haddock and profiling is mainly to speed up Nix builds.
+          haddock = lib.mkDefault false;
+          # Avoid double-compilation.
+          libraryProfiling = lib.mkDefault false;
+          separateBinOutput =
+            if package.cabal.executables == [ ]
+            then null
+            # The use of -fwhole-archive-hs-libs (see hpack/defaults.yaml)
+            # breaks builds on macOS (cyclic references between bin and out); this
+            # works around that.
+            else !pkgs.stdenv.isDarwin;
+        };
     };
   };
 }
