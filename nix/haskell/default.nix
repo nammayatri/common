90fc38c398503e0452e42a499fb78c17dd540a5c
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-10-17 13:22:32 +0530

    fix(redundant hpack defaults): removes the whole -fwhole-archive-hs-libs default
    * removes the "ghc-options: -fwhole-archive-hs-libs" default
      setting from hpack defaults from the file
      nix/haskell/hpack/defaults.yaml
    
    * modifies the setting `separateBinOutput` accordingly in
      the file nix/haskell/default.nix
    
    * keeps the machinery for specifying common hpack defaults intact,
      for future use, if it's truly redundant, it may be removed
      in a future iteration.

--- nix/haskell/default.nix
+++ nix/haskell/default.nix
@@ -12,13 +12,7 @@
           haddock = lib.mkDefault false;
           # Avoid double-compilation.
           libraryProfiling = lib.mkDefault false;
-          separateBinOutput =
-            if package.cabal.executables == [ ]
-            then null
-            # The use of -fwhole-archive-hs-libs (see hpack/defaults.yaml)
-            # breaks builds on macOS (cyclic references between bin and out); this
-            # works around that.
-            else !pkgs.stdenv.isDarwin;
+          separateBinOutput = package.cabal.executables != [ ];
         };
     };
   };
