6e58f51a5cd6a1a054c08752c69ba71dbc706eeb
shivaraj-bh <me@shivaraj-bh.in>
2024-11-12 15:28:18 +0530

    Remove `nix-health`

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
     nixpkgs-140774-workaround.url = "github:srid/nixpkgs-140774-workaround";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     cachix-push.url = "github:juspay/cachix-push";
-    nix-health.url = "github:juspay/nix-health";
 
     # Rust
     rust-overlay.url = "github:oxalica/rust-overlay";
